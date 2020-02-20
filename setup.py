97071ddb37a130f59db0fe9f71a15caa8d8282ed
Francesc Guitart Bravo <francesc.guitart.-nd@disneyresearch.com>
2020-02-20 15:15:28 +0100

    added instalation script

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "fastai_audio",
+    description = ("machine learning audio"),
+    license = "apache",
+    keywords = "machine learning audio",
+    url = "http://packages.python.org/an_example_pypi_project",
+    packages=['fastai_audio'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: machine learning audio",
+    ],
+)
